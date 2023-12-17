f71531126bfb9cd962d8ded597cb7d2c4e6e835b
rodionion <rodionvh@gmail.com>
2023-12-17 19:34:00 -0300

    Add dotenv

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 70;
 const path = require("path");
 const cors = require("cors");
+require("dotenv").config();
 // const axios = require("axios");
 const { logger } = require("./logger");
 const { sanitizeString } = require("./sanitizeString.js");
@@ -19,15 +20,48 @@ app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
-app.post("/api/email", (req, res) => {
-    let { name, email, phone, message } = req.body;
-    name = sanitizeString(name);
-    email = sanitizeString(email);
-    phone = sanitizeString(phone);
-    message = sanitizeString(message);
+app.post("/api/email", async (req, res) => {
 
-    transporter.sendMail(message);
+    // const { token } = req.body;
+    // const googleURL = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.CAPTCHA_SECRET_KEY}&response=${token}`;
 
+    // try {
+    //     const response = await axios.post(googleURL);
+    //     if (response.data.success) {
+    //         logger.info(`Captcha in ${req.url} successful`);
+
+            let { name, email, phone, formMessage } = req.body;
+            name = sanitizeString(name);
+            email = sanitizeString(email);
+            phone = sanitizeString(phone);
+            formMessage = sanitizeString(formMessage);
+
+            const emailBody = `<h2>New Message From The Website!</h2>
+            <p>Name: ${name}</p>
+            <p>Email: ${email}</p>
+            <p>Phone: ${phone}</p>
+            <p>Message: ${formMessage}</p>`;
+
+            const message = {
+                from: process.env.EMAIL_FROM,
+                to: process.env.EMAIL_TO,
+                subject: "New Message From Website",
+                html: emailBody
+            };
+
+            transporter.sendMail(message)
+                .then(() => res.json({success: true}))
+                .catch(() => res.json({success: false}));
+
+    //     } else {
+    //         logger.info("CAPTCHA failed");
+    //         res.json({res: "reCAPTCHA failed"});
+    //     }
+    // } catch (error) {
+    //     logger.info("CAPTCHA error");
+    //     logger.info(error);
+    //     res.status(500).json({res: "Error verifying reCAPTCHA"});
+    // }
 });
 
 app.listen(port, () => logger.info(`Listening to port ${port}`));
